a44038b0fc1d1c1753a06fe72434660d58321501
Pedro <pedrorhamon16@gmail.com>
2021-03-23 12:14:21 -0300

    Criando projeto

---
+++ src/main/java/com/startrek/servicesmc/ServicesmcApplication.java
@@ -0,0 +1,13 @@
+package com.startrek.servicesmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startrek/servicesmc/ServicesmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startrek.servicesmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
