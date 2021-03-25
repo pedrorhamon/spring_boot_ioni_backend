34d7a52146721316b004fcdcbf573a08af14eaa1
Pedro <pedrorhamon16@gmail.com>
2021-03-25 10:33:22 -0300

    Estado e cidade

--- src/main/java/com/startrek/servicesmc/ServicesmcApplication.java
+++ src/main/java/com/startrek/servicesmc/ServicesmcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.startrek.servicesmc.domain.Categoria;
+import com.startrek.servicesmc.domain.Cidade;
+import com.startrek.servicesmc.domain.Estado;
 import com.startrek.servicesmc.domain.Produto;
 import com.startrek.servicesmc.repositories.CategoriaRepository;
+import com.startrek.servicesmc.repositories.CidadeRepository;
+import com.startrek.servicesmc.repositories.EstadoRepository;
 import com.startrek.servicesmc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class ServicesmcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -42,8 +52,21 @@ public class ServicesmcApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+			
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Pernambuco");
+		Estado est2 = new Estado(null, "Paraiba");
+		
+		Cidade c1 = new Cidade(null, "Recife", est1);
+		Cidade c2 = new Cidade(null, "João Pessoa", est2);
+		Cidade c3 = new Cidade(null, "Campina Grande", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 }
