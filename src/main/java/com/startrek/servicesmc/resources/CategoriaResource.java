7a0ed8c937f28418beffadb830007b911b344c67
Pedro <pedro.rhamon@dataprev.gov.br>
2021-03-26 12:53:02 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/startrek/servicesmc/resources/CategoriaResource.java
+++ src/main/java/com/startrek/servicesmc/resources/CategoriaResource.java
@@ -44,4 +44,10 @@ public class CategoriaResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value ="/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> search(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/startrek/servicesmc/services/CategoriaService.java
+++ src/main/java/com/startrek/servicesmc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.startrek.servicesmc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.startrek.servicesmc.domain.Categoria;
 import com.startrek.servicesmc.repositories.CategoriaRepository;
+import com.startrek.servicesmc.services.exceptions.DateIntegrityException;
 import com.startrek.servicesmc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,4 +32,15 @@ public class CategoriaService {
 		search(obj.getId());
 		return repp.save(obj);
 	}
+	
+	public void delete(Integer id) {
+		search(id);
+		try {
+			repp.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DateIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
+	}
+		
 }
