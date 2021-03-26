7eb0e3e0f0a77dee47b8cdfc2142d23d9658aa11
Pedro <pedrorhamon16@gmail.com>
2021-03-26 13:17:43 -0300

    Listando as categorias com DTO

--- src/main/java/com/startrek/servicesmc/services/CategoriaService.java
+++ src/main/java/com/startrek/servicesmc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.startrek.servicesmc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,10 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DateIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repp.findAll();
 	}
 		
 }
