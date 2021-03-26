1c29b90f21b64d39def5a1f71703acec64969c76
Pedro <pedrorhamon16@gmail.com>
2021-03-26 01:55:46 -0300

    Inserindo uma nova Categoria POST

--- src/main/java/com/startrek/servicesmc/resources/CategoriaResource.java
+++ src/main/java/com/startrek/servicesmc/resources/CategoriaResource.java
@@ -1,14 +1,19 @@
 package com.startrek.servicesmc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.startrek.servicesmc.domain.Categoria;
 import com.startrek.servicesmc.services.CategoriaService;
+
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -21,5 +26,15 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.search(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
