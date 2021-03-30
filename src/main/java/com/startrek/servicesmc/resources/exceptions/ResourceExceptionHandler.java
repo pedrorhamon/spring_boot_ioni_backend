03e0fd27df625564e2acc235e43227866fc6dc24
Pedro <pedrorhamon16@gmail.com>
2021-03-30 09:28:46 -0300

    Personalizando validacoes

--- src/main/java/com/startrek/servicesmc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/startrek/servicesmc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,7 +19,6 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
-		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 	
@@ -25,7 +26,17 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> dateIntegrity(DateIntegrityException e, HttpServletRequest request){
 		
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(DateIntegrityException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
 		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de Validação", System.currentTimeMillis());
+		for(FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+			
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 
