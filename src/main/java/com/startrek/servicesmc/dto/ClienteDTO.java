c8c651115b3e1106d310cc7ec2557802975abbef
Pedro <pedrorhamon16@gmail.com>
2021-03-30 11:51:41 -0300

    Inserindo o novo cliente com POST

--- src/main/java/com/startrek/servicesmc/dto/ClienteDTO.java
+++ src/main/java/com/startrek/servicesmc/dto/ClienteDTO.java
@@ -55,7 +55,4 @@ public class ClienteDTO implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
-
 }
--- src/main/java/com/startrek/servicesmc/services/ClienteService.java
+++ src/main/java/com/startrek/servicesmc/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.startrek.servicesmc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.startrek.servicesmc.domain.Cidade;
 import com.startrek.servicesmc.domain.Cliente;
-import com.startrek.servicesmc.dto.ClienteDTO;
+import com.startrek.servicesmc.domain.Endereco;
+import com.startrek.servicesmc.domain.enums.TipoCliente;
+import com.startrek.servicesmc.dto.ClienteNewDTO;
 import com.startrek.servicesmc.repositories.ClienteRepository;
+import com.startrek.servicesmc.repositories.EnderecoRepository;
 import com.startrek.servicesmc.services.exceptions.DateIntegrityException;
 import com.startrek.servicesmc.services.exceptions.ObjectNotFoundException;
 
@@ -22,10 +28,21 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repp;
 	
+	@Autowired
+	private EnderecoRepository ender;
+	
 	public Cliente search(Integer id) {
 		Optional<Cliente> obj = repp.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException
 		("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));	
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repp.save(obj);
+		ender.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -53,8 +70,21 @@ public class ClienteService {
 		return repp.findAll(pageRequest);
 	}
 	
-	public Cliente fromDto(ClienteDTO objDto) {
-		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfouCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), 
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
